168ce6b47c1a7517f63e2e2e511c19aa5d82c458
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-22 09:28:56 -0600

    Created useState props

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import React, { useState } from 'react';
 
 export default function App() {
   let sum = 0;
-  const [numberOne, setNumberOne] = useState(0);
-  const [numberTwo, setNumberTwo] = useState(0);
+  const [numberOne, setNumberOne] = useState('');
+  const [numberTwo, setNumberTwo] = useState('');
+  const oneAsNumber = Number.parseInt(numberOne);
+  const twoAsNumber = Number.parseInt(numberTwo);
 
   function calculation() {
-
+    sum = {oneAsNumber} + {twoAsNumber};
+    return sum;
   }
 
   return (
     <>
       <h1>Simple Addition</h1>
-      <input id="numberOne" type="number" />
+      <input id="numberOne" type="number" value={numberOne} onChange={e => setNumberOne(e.target.value)} />
       &nbsp;
-      <input id="numberTwo" type="number" />
+      <input id="numberTwo" type="number" value={numberTwo} onChange={e => setNumberTwo(e.target.value)}/>
       <p>
-        The sum of these two numbers is:
+        The sum of these two numbers is:<span></span>
       </p>
 
     </>
