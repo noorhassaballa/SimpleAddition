89c679727c8b239dafe2088c3a1da3a5295d412f
Noor <145940214+noorhassaballa@users.noreply.github.com>
2025-01-22 20:24:22 -0600

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import React, { useState } from 'react';
 
 export default function App() {
-  let sum = 0;
-  const [numberOne, setNumberOne] = useState('');
-  const [numberTwo, setNumberTwo] = useState('');
-  const oneAsNumber = Number.parseInt(setNumberOne);
-  const twoAsNumber = Number.parseInt(setNumberTwo);
+  let [sum, setSum] = useState(0);
+  const [numberOne, setNumberOne] = useState(0);
+  const [numberTwo, setNumberTwo] = useState(0);
 
   function calculation() {
-    sum = oneAsNumber + twoAsNumber;
+    sum = numberOne + numberTwo;
   }
 
   calculation();
@@ -17,9 +15,9 @@ export default function App() {
   return (
     <>
       <h1>Simple Addition</h1>
-      <input id="numberOne" type="number" value={numberOne} onChange={e => setNumberOne(e.target.value)} />
+      <input id="numberOne" type="number" value={numberOne} onChange={e => setNumberOne(Number.parseInt(e.target.value))} />
       &nbsp;
-      <input id="numberTwo" type="number" value={numberTwo} onChange={e => setNumberTwo(e.target.value)}/>
+      <input id="numberTwo" type="number" value={numberTwo} onChange={e => setNumberTwo(Number.parseInt(e.target.value))} />
       <p>
         The sum of these two numbers is: <span>{sum}</span>
       </p>
